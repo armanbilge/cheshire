841721cff4d8abff32339a7c550fa8920c5fc9b6
Arman Bilge <armanbilge@gmail.com>
2022-03-23 03:29:14 +0000

    Parsers are hard?

--- newick/src/main/scala/cheshire/newick.scala
+++ newick/src/main/scala/cheshire/newick.scala
@@ -18,15 +18,42 @@ package cheshire
 package newick
 
 import cats.Eval
+import cats.parse.Parser
+import cats.parse.Parser0
+import cats.parse.Rfc5234
 import cats.syntax.all.*
 
 import scala.collection.mutable
+import cats.parse.Numbers
+import cats.data.NonEmptyList
 
 final case class Tree(subtree: Subtree)
 sealed abstract class Subtree
 final case class Leaf(name: Option[String]) extends Subtree
-final case class Internal(branchSet: List[Branch], name: Option[String]) extends Subtree
+final case class Internal(branchSet: NonEmptyList[Branch], name: Option[String]) extends Subtree
 final case class Branch(subtree: Subtree, length: Option[BigDecimal])
+
+private val subtree: Parser0[Subtree] = leaf | internal
+
+private val internal = (branch
+  .repSep(Parser.char(',').surroundedBy(skip))
+  .surroundedBy(skip)
+  .between(Parser.char('('), Parser.char(')')) ~ name.?).map(Internal(_, _))
+
+private val branch = ((subtree <* skip) ~ length.?).map(Branch(_, _))
+
+private val leaf = name.?.map(Leaf(_))
+
+private val name =
+  val unquoted =
+    Parser.charIn(('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9') ++ "#*%/.\\-+_&").repAs[String]
+  val quoted = (Rfc5234.vchar | Parser.charIn(' ')).repAs[String]
+  (unquoted | quoted.surroundedBy(Rfc5234.dquote) | quoted.surroundedBy(Parser.char('\'')))
+
+private val length = Parser.char(':') *> Numbers.jsonNumber.map(BigDecimal(_))
+
+private val skip = (comment.void | Parser.charIn(" \t\n").rep.void).rep0.void
+private val comment = Parser.anyChar.between(Parser.char('['), Parser.char(']'))
 
 def render(tree: Tree): String =
   val sb = new mutable.StringBuilder
